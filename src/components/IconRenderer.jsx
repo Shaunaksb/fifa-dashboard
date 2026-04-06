dfb37c7d05933070969272a3a34821e368ee0c00
Shaunaksb <43343551+Shaunaksb@users.noreply.github.com>
2026-04-06 16:43:13 +0530

    Updates

--- src/components/IconRenderer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import * as Icons from 'lucide-react';
-
-const IconRenderer = ({ name, ...props }) => {
-  const Icon = Icons[name] || Icons.HelpCircle;
-  return <Icon {...props} />;
-};
-
-export default IconRenderer;
